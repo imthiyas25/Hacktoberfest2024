4282dffec5570b4934b550295c94c456ab880419
Mohamed Imthiyas <115389114+imthiyas25@users.noreply.github.com>
2024-10-06 17:49:39 +0530

    Update prime number.js
    code efficient

--- prime number.js
+++ prime number.js
@@ -1,33 +1,18 @@
-// program to check if a number is prime or not
+function isPrime(n) {
+  // Corner case
+  if (n <= 1)
+    return false;
 
-// take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+  // Check from 2 to n-1
+  for (let i = 2; i < n; i++)
+    if (n % i == 0)
+      return false;
 
-// check if number is equal to 1
-if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
+  return true;
 }
 
-// check if number is greater than 1
-else if (number > 1) {
+// Driver Code
 
-    // looping through 2 to number/2
-    for (let i = 2; i <= number/2; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}
-
-// check if number is less than 1
-else {
-    console.log("The number is not a prime number.");
-}
+isPrime(11)
+  ? console.log("true")
+  : console.log("false");
